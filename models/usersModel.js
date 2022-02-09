d4489d18ce9453d8d854a67b59a2d1d0c43256bb
pierluigi <pierluigibaiano@gmail.com>
2022-02-09 16:47:23 +0100

    Define  usersModel

---
+++ models/usersModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import Joi from "joi";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    min: 3,
+    max: 255,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+const validate = (user) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(255).required(),
+    email: Joi.string().email().required(),
+  });
+  return schema.validate(user);
+};
+
+export default {
+  UserModel,
+  validate,
+};
